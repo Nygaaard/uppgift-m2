d3225456686454b319e23bf7630ce49874bf69ea
Nygaaard <nygardandreas@hotmail.com>
2025-02-10 15:06:22 +0100

    Activated MVC, static files and routing

--- Program.cs
+++ Program.cs
@@ -1,6 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
+
+//Aktivera MVC
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//Aktivera statiska filer
+app.UseStaticFiles();
+
+//Aktivera routing
+app.UseRouting();
+
+//Routing
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id}"
+);
 
 app.Run();

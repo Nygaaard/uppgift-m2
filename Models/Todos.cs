3dc5b8deb0f13a97577ef02e4bbb98b0711276ca
Nygaaard <nygardandreas@hotmail.com>
2025-02-11 08:58:21 +0100

    Created todo model

---
+++ Models/Todos.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace uppgiftm2.Models
+{
+    public class Todos
+    {
+        //Properties
+        public int? Id { get; set; }
+        public int? Title { get; set; }
+        public int? Description { get; set; }
+        public int? Status { get; set; }
+    }
+}
+
